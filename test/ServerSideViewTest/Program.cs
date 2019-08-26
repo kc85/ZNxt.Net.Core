633dfb21636795a6ae55e71097d9b552f43f1c98
kc85 <choudhury.khanin@gmail.com>
2019-08-26 16:52:43 +0530

    Added server side render test project

---
+++ test/ServerSideViewTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ZNxt.Net.Core.Web.ContentHandler;
+
+namespace ServerSideViewTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rv = new RazorTemplateEngine();
+            var data = rv.Compile($"Hello -- {DateTime.Now.ToString()} @Raw((1+1).ToString())", "aa", null);
+
+            Console.WriteLine("Hello World!" + data);
+            Console.Read();
+        }
+    }
+}
