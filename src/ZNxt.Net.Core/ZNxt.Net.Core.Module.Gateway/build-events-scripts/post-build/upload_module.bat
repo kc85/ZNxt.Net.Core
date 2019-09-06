69e0ae93cc08bd531cca339310ea5511c1fb79a6
kc85 <choudhury.khanin@gmail.com>
2019-09-06 19:23:12 +0530

    Update on gateway route

--- src/ZNxt.Net.Core/ZNxt.Net.Core.Module.Gateway/build-events-scripts/post-build/upload_module.bat
+++ src/ZNxt.Net.Core/ZNxt.Net.Core.Module.Gateway/build-events-scripts/post-build/upload_module.bat
@@ -1,8 +1,10 @@
 ﻿
 echo OFF
-set http_base_url=https://localhost:44373
+
+rem set http_base_url=https://localhost:44373
 
 rem set  http_base_url=http://gateway.znxt.fashion
+set http_base_url=http://s2ftechnologies.com
 
 wget -S -O uninstall_response.json --post-data "{\"Name\":\"ZNxt.Net.Core.Module.Gateway\"}" %http_base_url%/api/moduleinstaller/uninstall
 
