37b0842d318259a2a7adceb03fc82078ad7f6b4e
kc85 <choudhury.khanin@gmail.com>
2019-09-06 19:28:19 +0530

    Update on gateway route

---
+++ src/ZNxt.Net.Core/ZNxt.Net.Core.Web/Services/Api/Routes/LocalRoutesController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ZNxt.Net.Core.Consts;
+using ZNxt.Net.Core.Interfaces;
+using ZNxt.Net.Core.Model;
+
+
+namespace ZNxt.Net.Core.Web.Services.Api.Routes
+{
+    public class LocalRoutesController
+    {
+        private readonly IResponseBuilder _responseBuilder;
+        private readonly IRouting _routing;
+        public LocalRoutesController(IResponseBuilder responseBuilder, IRouting routing)
+        {
+            _responseBuilder = responseBuilder;
+            _routing = routing;
+        }
+        [Route("/local/routes", CommonConst.ActionMethods.GET, CommonConst.CommonValue.ACCESS_ALL)]
+        public async Task<JObject> Get()
+        {
+            var routes = _routing.GetRoutes();
+            var data = Newtonsoft.Json.JsonConvert.SerializeObject(routes);
+            return await Task.FromResult<JObject>(_responseBuilder.Success(JArray.Parse(data)));
+        }
+    }
+}
