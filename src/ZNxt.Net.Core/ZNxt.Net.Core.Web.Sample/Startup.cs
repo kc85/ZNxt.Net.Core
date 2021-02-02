dfe473d2454873bb7e5a581fb91c6d48e456fbb5
kc85 <choudhury.khanin@gmail.com>
2021-02-02 17:14:17 +0530

    added cors

--- src/ZNxt.Net.Core/ZNxt.Net.Core.Web.Sample/Startup.cs
+++ src/ZNxt.Net.Core/ZNxt.Net.Core.Web.Sample/Startup.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.HttpOverrides;
+using ZNxt.Net.Core.Helpers;
 
 namespace ZNxt.Net.Core.Web.Sample
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public IHostingEnvironment Environment { get; }
         public IConfiguration Configuration { get; }
         public Startup(IHostingEnvironment environment, IConfiguration configuration)
@@ -17,11 +20,13 @@ namespace ZNxt.Net.Core.Web.Sample
         }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
             services.AddZNxtSSO(Environment);
             services.AddZNxtApp();
             services.AddZNxtBearerAuthentication();
             services.AddZNxtIdentityServer();
+           
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -37,7 +42,14 @@ namespace ZNxt.Net.Core.Web.Sample
             fordwardedHeaderOptions.KnownProxies.Clear();
 
             app.UseForwardedHeaders(fordwardedHeaderOptions);
-
+            var corurl = CommonUtility.GetAppConfigValue("cor_urls");
+            if (string.IsNullOrEmpty(corurl))
+            {
+                corurl = "http://localhost:50071";
+            }
+            app.UseCors(
+                    options => options.WithOrigins(corurl.Split(';')).AllowAnyMethod()
+             );
             app.UseZNxtSSO();
             app.UseAuthentication();
             app.UseZNxtApp();
